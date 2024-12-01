59c26f939db06fe3c7f6526f2ee09e0ccd4de98a
NikhilDaivanapally <nikhildaivanapally@gmail.com>
2024-12-01 08:58:23 +0530

    added NoProductsFound component

--- client/src/pages/Products.page.tsx
+++ client/src/pages/Products.page.tsx
@@ -24,6 +24,7 @@ import FilterLoader from "../components/Loaders/FilterLoader";
 import Pagination from "../components/Pagination/Pagination";
 import { queryInterface } from "../TsInterfaces/Interfaces";
 import { UpdateTotalProductsCount } from "../store/slices/productsSlice";
+import NoProductsFound from "../components/NoProductsFound/NoProductsFound";
 
 const Products = () => {
   const Navigate = useNavigate();
@@ -159,124 +160,128 @@ const Products = () => {
   return (
     <>
       {!isLoading ? (
-        <div className="p-3 relative md:p-6 flex flex-col gap-0 w-full">
-          {isFilterLoading && <FilterLoader />}
-          {/* breadCrums */}
-          <BreadCrums />
-          {/* products count */}
-          <div className="hidden md:flex md:w-fit gap-2 bg-white sticky md:top-24">
-            <p>
-              {ModifiedCurrentPathArrayValues[0]}{" "}
-              {
-                ModifiedCurrentPathArrayValues[
-                  ModifiedCurrentPathArrayValues.length - 1
-                ]
-              }{" "}
-            </p>{" "}
-            -{" "}
-            <p className="text-gray-500">
-              {productsAndCount?.data?.productsCount} items
-            </p>
-          </div>
-          {/*view As &  sort */}
-          <div className=" w-full hidden md:flex items-center md:justify-end gap-1  md:gap-10">
-            <div className="flex md:gap-2 items-center">
-              <p>View As : </p>
-              <div className="flex gap-2">
-                {[[...Array(2)], [...Array(3)], [...Array(4)]].map(
-                  (view, i) => {
-                    return (
-                      <div
-                        key={i}
-                        onClick={() => handleSelectGridview(view.length)}
-                        className="cursor-pointer border-2 w-fit flex gap-1 p-1 h-8"
-                      >
-                        {view.map((_, i) => (
-                          <div
-                            key={i}
-                            className={`h-full w-1 ${
-                              view.length == gridview
-                                ? "bg-black"
-                                : "bg-gray-300"
-                            }`}
-                          ></div>
-                        ))}
-                      </div>
-                    );
-                  }
+        productsAndCount?.data.productsCount ? (
+          <div className="p-3 relative md:p-6 flex flex-col gap-0 w-full">
+            {isFilterLoading && <FilterLoader />}
+            {/* breadCrums */}
+            <BreadCrums />
+            {/* products count */}
+            <div className="hidden md:flex md:w-fit gap-2 bg-white sticky md:top-24">
+              <p>
+                {ModifiedCurrentPathArrayValues[0]}{" "}
+                {
+                  ModifiedCurrentPathArrayValues[
+                    ModifiedCurrentPathArrayValues.length - 1
+                  ]
+                }{" "}
+              </p>{" "}
+              -{" "}
+              <p className="text-gray-500">
+                {productsAndCount?.data?.productsCount} items
+              </p>
+            </div>
+            {/*view As &  sort */}
+            <div className=" w-full hidden md:flex items-center md:justify-end gap-1  md:gap-10">
+              <div className="flex md:gap-2 items-center">
+                <p>View As : </p>
+                <div className="flex gap-2">
+                  {[[...Array(2)], [...Array(3)], [...Array(4)]].map(
+                    (view, i) => {
+                      return (
+                        <div
+                          key={i}
+                          onClick={() => handleSelectGridview(view.length)}
+                          className="cursor-pointer border-2 w-fit flex gap-1 p-1 h-8"
+                        >
+                          {view.map((_, i) => (
+                            <div
+                              key={i}
+                              className={`h-full w-1 ${
+                                view.length == gridview
+                                  ? "bg-black"
+                                  : "bg-gray-300"
+                              }`}
+                            ></div>
+                          ))}
+                        </div>
+                      );
+                    }
+                  )}
+                </div>
+              </div>
+              <button className="w-fit p-2 border-2 border-black">
+                Sort by Recommend ▼
+              </button>
+            </div>
+            {/* mobile filter */}
+            <div className="sticky flex items-center md:hidden top-[70px] w-full h-10 bg-white z-10">
+              <button
+                className=" w-full h-full flex items-center justify-center gap-2 text-lg border-[1.5px]"
+                onClick={handleMobileFilter}
+              >
+                <BiFilterAlt />
+                Filter
+              </button>
+              <button className="w-full h-full  flex items-center justify-center gap-2 text-lg border-[1.5px]">
+                <BiSort />
+                Sort
+              </button>
+
+              {/* small screen */}
+              <div
+                className={`w-full ${
+                  ismobilefilter ? "block" : "hidden"
+                } absolute top-0 z-20 bg-white  flex flex-col md:hidden h-[87.5vh]`}
+              >
+                <RxCross2
+                  className="ml-auto text-5xl p-3"
+                  onClick={handleMobileFilter}
+                />
+                <Filters
+                  query={query}
+                  setQuery={setQuery}
+                  BrandsData={brandsAndPrices?.data?.brands}
+                  PricesData={brandsAndPrices?.data?.priceRange}
+                />
+              </div>
+              {/* )} */}
+            </div>
+            {/*filter &  Products view */}
+            <div className="w-full flex">
+              {/* medium & large screen */}
+              <div className="hidden md:block w-[280px] border-[1px] border-l-transparent">
+                <Filters
+                  query={query}
+                  setQuery={setQuery}
+                  BrandsData={brandsAndPrices?.data?.brands}
+                  PricesData={brandsAndPrices?.data?.priceRange}
+                />
+              </div>
+
+              <div className="flex-1 flex flex-col gap-6">
+                <ProductsDisplay
+                  gridview={gridview}
+                  productsData={productsAndCount?.data?.products}
+                  triggerdeleteProduct={triggerdeleteProduct}
+                  deleteProductIsLoading={deleteProductIsLoading}
+                />
+
+                {productsAndCount?.data?.productsCount &&
+                productsAndCount?.data?.productsCount > 0 ? (
+                  <Pagination
+                    query={query}
+                    setQuery={setQuery}
+                    ProductsCount={productsAndCount?.data?.productsCount}
+                  />
+                ) : (
+                  <></>
                 )}
               </div>
             </div>
-            <button className="w-fit p-2 border-2 border-black">
-              Sort by Recommend ▼
-            </button>
           </div>
-          {/* mobile filter */}
-          <div className="sticky flex items-center md:hidden top-[70px] w-full h-10 bg-white z-10">
-            <button
-              className=" w-full h-full flex items-center justify-center gap-2 text-lg border-[1.5px]"
-              onClick={handleMobileFilter}
-            >
-              <BiFilterAlt />
-              Filter
-            </button>
-            <button className="w-full h-full  flex items-center justify-center gap-2 text-lg border-[1.5px]">
-              <BiSort />
-              Sort
-            </button>
-
-            {/* small screen */}
-            <div
-              className={`w-full ${
-                ismobilefilter ? "block" : "hidden"
-              } absolute top-0 z-20 bg-white  flex flex-col md:hidden h-[87.5vh]`}
-            >
-              <RxCross2
-                className="ml-auto text-5xl p-3"
-                onClick={handleMobileFilter}
-              />
-              <Filters
-                query={query}
-                setQuery={setQuery}
-                BrandsData={brandsAndPrices?.data?.brands}
-                PricesData={brandsAndPrices?.data?.priceRange}
-              />
-            </div>
-            {/* )} */}
-          </div>
-          {/*filter &  Products view */}
-          <div className="w-full flex">
-            {/* medium & large screen */}
-            <div className="hidden md:block w-[280px] border-[1px] border-l-transparent">
-              <Filters
-                query={query}
-                setQuery={setQuery}
-                BrandsData={brandsAndPrices?.data?.brands}
-                PricesData={brandsAndPrices?.data?.priceRange}
-              />
-            </div>
-
-            <div className="flex-1 flex flex-col gap-6">
-              <ProductsDisplay
-                gridview={gridview}
-                productsData={productsAndCount?.data?.products}
-                triggerdeleteProduct={triggerdeleteProduct}
-                deleteProductIsLoading={deleteProductIsLoading}
-              />
-
-              {productsAndCount?.data?.productsCount &&
-              productsAndCount?.data?.productsCount > 0 ? (
-                <Pagination
-                  query={query}
-                  setQuery={setQuery}
-                  ProductsCount={productsAndCount?.data?.productsCount}
-                />
-              ) : (
-                <></>
-              )}
-            </div>
-          </div>
-        </div>
+        ) : (
+          <NoProductsFound />
+        )
       ) : (
         <PageLoader />
       )}
